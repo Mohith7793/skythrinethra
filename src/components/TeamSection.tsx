15aa8b3a39eaa10c96e4fce0bd272accbd640d6b
Mohith7793 <138747877+Mohith7793@users.noreply.github.com>
2025-05-27 12:07:51 +0530

    Update TeamSection.tsx

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -45,7 +45,7 @@ const teamMembers: TeamMember[] = [
     name: "Ramana",
     position: "CFO |Backend Team Lead  ",
     bio: "Building robust and scalable backend systems with hands-on expertise in high-performance architecture and efficient data management",
-    avatar: "https://skythrinethrssets.blob.core.windows.net/skythrinethfiles/rmn.png?sp=r&st=2025-05-24T12:07:38Z&se=2027-03-28T20:07:38Z&spr=https&sv=2024-11-04&sr=b&sig=XysSOTmsdyPu8SQkQxTrbpRLcrLa8MGvU6ERt8qdo4Q%3D",
+    avatar: "https://skythrinethrssets.blob.core.windows.net/skythrinethfiles/ramana.jpg?sp=r&st=2025-05-27T05:50:40Z&se=2027-03-29T13:50:40Z&spr=https&sv=2024-11-04&sr=b&sig=jp4pc3DaqCABNXGnb%2BJI3K%2BV7hK8N7qdR2T4ol3E6To%3D",
     socialLinks: {
       linkedin: "https://www.linkedin.com/in/%EF%BD%93%EF%BD%8B%EF%BD%99%EF%BD%94%EF%BD%88%EF%BD%92%EF%BD%89%EF%BD%8E%CE%BE%EF%BD%94%EF%BD%88%EF%BD%92%CE%BB-274198355/",
       twitter: "https://x.com/i/flow/login?redirect_after_login=%2FSkyThrinethra",
